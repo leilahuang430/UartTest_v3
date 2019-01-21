0d5dbae613f2b314d78118dff6bacca0bd946171
leila_huang <leila_huang@askey.com.tw>
2019-01-21 16:55:31 +0800

    add UI button for re-open port, ttyGS0 and ttyGS1

--- app/src/main/java/app/com/askey/uarttest/UartTestMain.java
+++ app/src/main/java/app/com/askey/uarttest/UartTestMain.java
@@ -27,17 +27,15 @@ import android_serialport_api.SerialPort;
 
 public class UartTestMain extends Activity {
     private static final String TAG = "UartTestMain";
-
-    //UART
     protected SerialPort mSerialPort;
     protected InputStream mInputStream;
     protected OutputStream mOutputStream;
     private RadioGroup mRadioGroup;
-    private RadioButton mRadioButton0, mRadioButton1, mRadioButton2;
-    private Button mWriteBtn, mReadBtn, mClearBtn;
+    private RadioButton mRadioButton0, mRadioButton1, mRadioButton2, mRadioButton3, mRadioButton4;
+    private Button mWriteBtn, mReadBtn, mClearBtn, mReOpenBtn;
     private EditText minputEditText, mEditTextReception;
     private String mInputStr;
-    private int flag=3;
+    private int flag=5;
 
     //GPIO TEST
     //private String gpio10_path = "/dev/READING_PW_EN";  //set 0=Low  1=high,  control Level Shift
@@ -45,7 +43,6 @@ public class UartTestMain extends Activity {
 
     //private String gpio_test= "/dev/EXTERNAL_PORT_EN";  //SAM TEST
     //GPIO END
-
     private ScrollView mScrollView;
     private TextView mLog;
 
@@ -56,6 +53,8 @@ public class UartTestMain extends Activity {
         mRadioButton0 = (RadioButton) findViewById(R.id.port_ttyHSL0);
         mRadioButton1 = (RadioButton) findViewById(R.id.port_ttyHSL1);
         mRadioButton2 = (RadioButton) findViewById(R.id.port_ttyHSL2);
+        mRadioButton3 = (RadioButton) findViewById(R.id.port_ttyGS0);
+        mRadioButton4 = (RadioButton) findViewById(R.id.port_ttyGS1);
         mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
         mRadioGroup.setOnCheckedChangeListener(radGrpRegionOnCheckedChange);
         minputEditText = (EditText) findViewById(R.id.editText1);
@@ -65,6 +64,7 @@ public class UartTestMain extends Activity {
         mWriteBtn = (Button) findViewById(R.id.write_btn);
         mReadBtn = (Button) findViewById(R.id.read_btn);
         mClearBtn = (Button) findViewById(R.id.clear_btn);
+        mReOpenBtn = (Button) findViewById(R.id.reopen_btn);
 
         mScrollView = (ScrollView) findViewById(R.id.scrollView);
         mLog = (TextView) findViewById(R.id.TextView2);
@@ -82,18 +82,23 @@ public class UartTestMain extends Activity {
                 mInputStr = minputEditText.getText().toString();
                 switch (flag) {
                     case 0:
-                        writeToFile("/dev/ttyHSL0", mInputStr);
+                        sendSerialPort("/dev/ttyHSL0", mInputStr);
                         break;
                     case 1:
-                        writeToFile("/dev/ttyHSL1", mInputStr);
+                        sendSerialPort("/dev/ttyHSL1", mInputStr);
                         break;
                     case 2:
-                        writeToFile("/dev/ttyHSL2", mInputStr);
+                        sendSerialPort("/dev/ttyHSL2", mInputStr);
+                        break;
+                    case 3:
+                        sendSerialPort("/dev/ttyGS0", mInputStr);
+                        break;
+                    case 4:
+                        sendSerialPort("/dev/ttyGS1", mInputStr);
                         break;
                 }
             }
         });
-
 
         mReadBtn.setOnClickListener(new OnClickListener() {
             @Override
@@ -108,6 +113,12 @@ public class UartTestMain extends Activity {
                     case 2:
                         readFile("/dev/ttyHSL2");
                         break;
+                    case 3:
+                        readFile("/dev/ttyGS0");
+                        break;
+                    case 4:
+                        readFile("/dev/ttyGS1");
+                        break;
                 }
             }
         });
@@ -118,7 +129,37 @@ public class UartTestMain extends Activity {
                 mEditTextReception.setText("");
                 mLog.setText("");
             }
+        });
 
+        mReOpenBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (mSerialPort != null) {
+                    mLog.append("[Return] uart is already open" +"\n"+mSerialPort+"\n");
+                    mScrollView.fullScroll(View.FOCUS_DOWN);
+                    Log.v(TAG, "[Return] uart is already open"+"\n");
+                    Log.v(TAG, mSerialPort +"\n");
+                } else {
+                    Log.e(TAG, "re open"+"\n");
+                    switch (flag) {
+                        case 0:
+                            openSerialPort("/dev/ttyHSL0",115200);
+                            break;
+                        case 1:
+                            openSerialPort("/dev/ttyHSL1",115200);
+                            break;
+                        case 2:
+                            openSerialPort("/dev/ttyHSL2",115200);
+                            break;
+                        case 3:
+                            openSerialPort("/dev/ttyGS0",9600);
+                            break;
+                        case 4:
+                            openSerialPort("/dev/ttyGS1",9600);
+                            break;
+                    }
+                }
+            }
         });
     }
 
@@ -129,85 +170,125 @@ public class UartTestMain extends Activity {
                 case R.id.port_ttyHSL0:
                     Log.v(TAG, "choose port_ttyHSL0");
                     flag=0;
-                    try {
-                        mSerialPort = new SerialPort(new File("/dev/ttyHSL0"), 115200, 0);
-                        mInputStream = mSerialPort.getInputStream();
-                        mOutputStream = mSerialPort.getOutputStream();
-                        mLog.append("open UART : " + "/dev/ttyHSL0" + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
-                    } catch (SecurityException e) {
-                        e.printStackTrace();
-                        mLog.append("open  UART  failed SecurityException : " + e + "\n");
-                        Log.e(TAG, "open  UART failed SecurityException : " + e);
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "open  UART failed IOException : " + e);
-                        mLog.append("open  UART failed IOException : " + e + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
+                    if (mSerialPort != null) {
+                        closeSerialPort();
                     }
-                    Toast.makeText(getApplicationContext(), "open /dev/ttyHSL0",Toast.LENGTH_SHORT).show();
+                    openSerialPort("/dev/ttyHSL0",115200);
+
+                    Toast.makeText(getApplicationContext(), "choose /dev/ttyHSL0",Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.port_ttyHSL1:
                     Log.v(TAG, "choose port_ttyHSL1");
                     flag=1;
-                    try {
-                        mSerialPort = new SerialPort(new File("/dev/ttyHSL1"), 115200, 0);
-                        mInputStream = mSerialPort.getInputStream();
-                        mOutputStream = mSerialPort.getOutputStream();
-                        mLog.append("open  UART : " + "/dev/ttyHSL1" + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
-
-                    } catch (SecurityException e) {
-                        e.printStackTrace();
-                        mLog.append("open  UART failed SecurityException : " + e + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
-                        Log.e(TAG, "open  UART failed SecurityException : " + e);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "open  UART failed IOException : " + e);
-                        mLog.append("open  UART failed IOException : " + e + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
+                    if (mSerialPort != null) {
+                        closeSerialPort();
                     }
-                    Toast.makeText(getApplicationContext(), "open /dev/ttyHSL1",Toast.LENGTH_SHORT).show();
+                    openSerialPort("/dev/ttyHSL1",115200);
+
+                    Toast.makeText(getApplicationContext(), "choose /dev/ttyHSL1",Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.port_ttyHSL2:
                     Log.v(TAG, "choose port_ttyHSL2");
                     flag=2;
-                    try {
-                        mSerialPort = new SerialPort(new File("/dev/ttyHSL2"), 115200, 0);
-                        mInputStream = mSerialPort.getInputStream();
-                        mOutputStream = mSerialPort.getOutputStream();
-                        mLog.append("open  UART : " + "/dev/ttyHSL2" + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
-                    } catch (SecurityException e) {
-                        e.printStackTrace();
-                        mLog.append("open  UART failed SecurityException : " + e + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
-                        Log.e(TAG, "open  UART failed SecurityException : " + e);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "open  UART failed IOException : " + e);
-                        mLog.append("open  UART failed IOException : " + e + "\n");
-                        mScrollView.fullScroll(View.FOCUS_DOWN);
+                    if (mSerialPort != null) {
+                        closeSerialPort();
                     }
-                    Toast.makeText(getApplicationContext(), "open /dev/ttyHSL2",Toast.LENGTH_SHORT).show();
+                    openSerialPort("/dev/ttyHSL2",115200);
+
+                    Toast.makeText(getApplicationContext(), "choose /dev/ttyHSL2",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.port_ttyGS0:
+                    Log.v(TAG, "choose port_ttyGS0");
+                    flag=3;
+                    if (mSerialPort != null) {
+                        closeSerialPort();
+                    }
+                    openSerialPort("/dev/ttyGS0",9600);
+
+                    Toast.makeText(getApplicationContext(), "choose /dev/ttyGS0",Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.port_ttyGS1:
+                    Log.v(TAG, "choose port_ttyGS0");
+                    flag=4;
+                    if (mSerialPort != null) {
+                        closeSerialPort();
+                    }
+                    openSerialPort("/dev/ttyGS1",9600);
+
+                    Toast.makeText(getApplicationContext(), "choose /dev/ttyGS1",Toast.LENGTH_SHORT).show();
                     break;
             }
         }
     };
 
-    private void writeToFile(String path, String stBuffer ) {
+    private void openSerialPort(String SerialPortpath, int baudrate) {
+        if (mSerialPort == null) {
+            try {
+                mSerialPort = new SerialPort(new File(SerialPortpath), baudrate, 0);
+                mInputStream = mSerialPort.getInputStream();
+                mOutputStream = mSerialPort.getOutputStream();
+
+                mLog.append("open  : " + SerialPortpath + "\n");
+                mLog.append(mSerialPort +"\n");
+                mScrollView.fullScroll(View.FOCUS_DOWN);
+                Log.v(TAG, "open  : " + SerialPortpath + "\n");
+                Log.v(TAG, mSerialPort+"\n");
+            } catch (SecurityException e) {
+                e.printStackTrace();
+
+                mLog.append("open failed SecurityException : " + e + "\n");
+                mLog.append("SerialPort : " +mSerialPort + "\n");
+                mScrollView.fullScroll(View.FOCUS_DOWN);
+                Log.e(TAG, "open  failed SecurityException : " + e);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                mLog.append("open failed IOException : " + e +"\n");
+                mLog.append("SerialPort : " +mSerialPort + "\n");
+                mScrollView.fullScroll(View.FOCUS_DOWN);
+                Log.e(TAG, "open  failed IOException : " + e);
+            }
+        }
+    }
+
+    public void closeSerialPort() {
         try {
-            mOutputStream.write(new String(stBuffer).getBytes());
-            mOutputStream.write('\n');
-            mLog.append(stBuffer+" write to UART: " + path + "\n");
-            mScrollView.fullScroll(View.FOCUS_DOWN);
-            Log.v(TAG, stBuffer+" write to UART: " + path);
+            if (mInputStream != null) {
+                mInputStream.close();
+                mInputStream = null;
+            }
+            if (mOutputStream != null) {
+                mOutputStream.close();
+                mOutputStream = null;
+            }
+            if (mSerialPort != null){
+                mSerialPort.close();
+                mSerialPort = null;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Toast.makeText(getApplicationContext(), stBuffer,Toast.LENGTH_SHORT).show();
+    }
+
+    public void sendSerialPort(String path, String data) {
+        if (mSerialPort == null){
+            mLog.append("my SerialPort is null: " +mSerialPort+ "\n");
+            mScrollView.fullScroll(View.FOCUS_DOWN);
+        } else {
+            try {
+                mOutputStream.write(new String(data).getBytes());
+                mOutputStream.flush();
+                mLog.append(data+" __write to : " + path + "\n");
+                mScrollView.fullScroll(View.FOCUS_DOWN);
+
+                Log.v(TAG, data+" __write to : " + path + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+                mLog.append(e + "  __ IOException " + "\n");
+                Log.e(TAG, e + "  __ IOException ");
+            }
+            Toast.makeText(getApplicationContext(), data,Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void readFile(String path) {
@@ -232,10 +313,10 @@ public class UartTestMain extends Activity {
                     String recData = new String(buffer, 0, size );
                     if (recData == "") {
                         mEditTextReception.append("UART error" + "\n");
-                        Log.d(TAG, "UART recData = "+ "error");
+                        Log.e(TAG, "UART recData = "+ "error");
                     } else {
                         mEditTextReception.append(recData);
-                        Log.d(TAG, "UART recData = "+ recData.getBytes());
+                        Log.v(TAG, "UART recData = "+ recData.getBytes());
                     }
                 }
             }
@@ -245,7 +326,7 @@ public class UartTestMain extends Activity {
 
     /**
      * GPIO  RWtest function
-     * line 227~268
+     * line 325~370
      */
     //GPIO  create file & write
     public void writeToGpio(String data, String gpioPath) {
@@ -256,7 +337,7 @@ public class UartTestMain extends Activity {
             writer.close();
             mLog.append("GPIO write = " + data +" to " + gpioPath + "\n");
             mScrollView.fullScroll(View.FOCUS_DOWN);
-            Log.d(TAG, "GPIO write = " + data +" to " + gpioPath);
+            Log.v(TAG, "GPIO write = " + data +" to " + gpioPath);
         } catch (IOException e1) {
             e1.printStackTrace();
             Log.e(TAG, "GPIO writeData IOException=" + e1);
@@ -291,15 +372,10 @@ public class UartTestMain extends Activity {
     }
 
     protected void onDestroy() {
-
 //        writeToGpio("0", gpio10_path);  // "1"= high , "0"= Low
 //        writeToGpio("0", gpio89_path);   //KEY_TOP_1, gpio89
 //        writeToGpio("0", gpio_test);   //SAM TEST
-
-        if (mSerialPort != null){
-            mSerialPort.close();
-            mSerialPort = null;
-        }
+        closeSerialPort();
         super.onDestroy();
     }
 
